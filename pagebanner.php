e50835879d0ce2f4c2286b714e92505354c3f275
Guy <guy.ads@gmail.com>
2017-03-14 20:46:47 +0000

    CSS update

--- pagebanner.php
+++ pagebanner.php
@@ -1,8 +1,8 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>לגדל ולאכול</h1>
-        <p>כלים לגנן הבייתי שמגדל בשביל לאכול...</p>
-        <p> האתר בתהליך הקמה - נשמח לראות אותך שוב בקרוב.</p> 
+        <h1>ניסוי בגידול ירקות ירוקים ושורש</h1>
+        <p>מוזמנים לבצטרף למסע אישי, לשתף ולהשתתף</p>
+        <p>בלוג אישי - Grows Today.....</p> 
       </div>
     </div>
